53b1bdf8d8cead9ab497952b8c01bf77f75b9598
Robins <robinmgk077@gmail.com>
2024-03-15 22:21:12 +0300

    Completed Assignment

--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+from sqlalchemy import create_engine
 from sqlalchemy import (Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class Dog(Base):
     __tablename__ = 'dogs'
@@ -11,3 +13,4 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
